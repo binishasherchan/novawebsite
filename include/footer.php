f0c161df35774a0250debeefff7b3e81f6d15554
binisha <sherchanbinisha5@gmail.com>
2023-08-08 17:11:23 +0545

    Add php  files

---
+++ include/footer.php
@@ -0,0 +1,69 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Our Location</h3>
+            <p>
+              Mahendrapul, <br>
+             Pokhara Kaski<br>
+              United States <br><br>
+              <strong>Phone:</strong>061-544199
+              <br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Computer Courses</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Study in Japan</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Study in Japan</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">English Language</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
+
+         
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          &copy; Copyright <strong><span>Company</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/company-free-html-bootstrap-template/ -->
+          Developed by Binisha Sherchan
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="https://www.facebook.com/NovaJapaneseLanguageInstitute" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram" target="_blank"><i class="bx bxl-instagram"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
---
+++ include/navbar.php
@@ -0,0 +1,35 @@
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.php"><img src="assets/img/nova-logo.png" alt=""></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.php" class="active">Home</a></li>
+
+          <li><a href="about.php">About</a>
+    
+          </li>
+
+          <li><a href="services.php">Services</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="contact.php">Contact</a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links d-flex">
+        <!-- <a href="#" class="twitter"><i class="bu bi-twitter"></i></a> -->
+        <a href="https://www.facebook.com/NovaJapaneseLanguageInstitute"  target="_blank" class="facebook"><i class="bu bi-facebook"></i></a>
+        <!-- <a href="#" class="instagram"><i class="bu bi-instagram"></i></a> -->
+        <a href=""> <i class="bu bi-instagram"></i></a>
+        <a href=""> <i class="bu bi-tiktok"></i></a>
+       
+      </div>
+
+    </div>
+  </header><!-- End Header -->
